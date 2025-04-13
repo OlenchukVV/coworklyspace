2c2426bb379d4e71dc27f012fc6bf4b162b34245
newwels <90894321+OlenchukVV@users.noreply.github.com>
2025-04-13 19:35:21 +0300

    Add files via upload

---
+++ js/dashboard-auth.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWwKso4qEdRK1SnWHxawP7Zm49BwcZz50",
+  authDomain: "coworklyspace.firebaseapp.com",
+  projectId: "coworklyspace",
+  storageBucket: "coworklyspace.appspot.com",
+  messagingSenderId: "1039847178271",
+  appId: "1:1039847178271:web:9fbece3255c14b5217d52a"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const userEmail = document.getElementById("userEmail");
+const logoutBtn = document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    userEmail.textContent = user.email;
+  } else {
+    window.location.href = "auth.html";
+  }
+});
+
+logoutBtn.addEventListener("click", () => {
+  signOut(auth).then(() => {
+    window.location.href = "index.html";
+  });
+});
